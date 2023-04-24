4d08ff007fe3c9bf22ea8699060882f628a334ee
Greg Griffith <griffith_g@hotmail.com>
2023-04-24 18:12:46 -0400

    uploaded solution

---
+++ 13-RomanToInteger/Solution.js
@@ -0,0 +1,32 @@
+//import modules and libraries
+
+//define roman numerals as key value pairs
+const key = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+
+//define function that takes a string of roman numerals and returns an integer
+const romanToInteger = (romanNumeral) => {
+ 
+    //define var to store running total
+    let sum = 0; 
+
+    //iterate thru roman numeral string
+    for (let i = 0; i < romanNumeral.length; i++) {
+        
+        //assign names to first two values
+        let firstNum = key[romanNumeral[i]];
+        let secondNum = key[romanNumeral[i+1]] || 0;
+        
+        //if first num is greater, add it to the sum
+        if (firstNum >= secondNum){
+            sum += firstNum;
+        } 
+        //if second number is greater, subtract the first num from it, then add to sum
+        else {
+            sum += secondNum - firstNum;
+            i++;        
+        }
+    }
+    
+    return sum; 
+}
+console.log(romanToInteger('LVIII'));
