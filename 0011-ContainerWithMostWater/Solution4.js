87519c82b719f2429c4d5aa1785bb86fc9b968b1
Greg Griffith <griffith_g@hotmail.com>
2023-07-22 17:47:04 -0400

    Leetcode 11 - Container with most water. uploaded 2 solutions

--- 0011-ContainerWithMostWater/Solution4.js
+++ 0011-ContainerWithMostWater/Solution4.js
@@ -6,15 +6,15 @@ var maxArea = function(height) {
     let max = 0, i = 0, j = height.length - 1;
     let bigI = i, bigJ = j;
     while(i < j){
-        if(height[i] < height[bigI]){
-            i++;
-            continue;
-        }
-        else if(height[j] < height[bigJ]){
-            j--;
-            continue;
-        }
+        let iSkip = height[i] < height[bigI];
+        let jSkip = height[j] < height[bigJ];
         
+        if(iSkip || jSkip){
+            if (iSkip) i++;
+            if (jSkip) j--;
+            continue;
+        }
+       
         let area = Math.min(height[i], height[j]) * (j - i);
         if(area > max){
             max = area;
